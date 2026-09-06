5e495a8a20179f55b0bb7e1b143371cf13bbfd68
darkimusss <itsden4ikwat@gmail.com>
2026-09-06 22:40:45 +0300

    feat(seo): add thaumcraft aspects and minecraft color generator routes to sitemap.xml

--- server/index.ts
+++ server/index.ts
@@ -912,6 +912,8 @@ app.get('/sitemap.xml', (_req, res) => {
 
     const urls = [
       `  <url>\n    <loc>${baseUrl}/</loc>\n    <changefreq>daily</changefreq>\n    <priority>1.0</priority>\n  </url>`,
+      `  <url>\n    <loc>${baseUrl}/thaumcraft</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.9</priority>\n  </url>`,
+      `  <url>\n    <loc>${baseUrl}/minecraft-color</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.9</priority>\n  </url>`,
       `  <url>\n    <loc>${baseUrl}/team</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.5</priority>\n  </url>`,
       `  <url>\n    <loc>${baseUrl}/rules</loc>\n    <changefreq>monthly</changefreq>\n    <priority>0.3</priority>\n  </url>`
     ];
