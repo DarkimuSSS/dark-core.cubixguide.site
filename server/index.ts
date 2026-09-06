4bb2b1c9e6ed7da534f55bfe30e09433a3163ec5
darkimusss <itsden4ikwat@gmail.com>
2026-09-06 23:16:36 +0300

    feat: add .well-known/discord verification route

--- server/index.ts
+++ server/index.ts
@@ -934,7 +934,12 @@ app.post('/api/server-rules', (req, res) => {
   }
 });
 
-// SEO & Sitemap Endpoints
+// SEO & Well-Known Endpoints
+app.get('/.well-known/discord', (_req, res) => {
+  res.type('text/plain');
+  res.send('dh=60731f855d4b5ddc2bf21422784534ef02868207');
+});
+
 app.get('/robots.txt', (_req, res) => {
   res.type('text/plain');
   res.send(
