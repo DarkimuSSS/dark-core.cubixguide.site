60575b97345e37a950438f4e5c2614b3574baf01
darkimusss <itsden4ikwat@gmail.com>
2026-09-10 11:51:04 +0300

    fix: import watch from vue in LayerPainter.vue to resolve ReferenceError

--- src/components/LayerPainter.vue
+++ src/components/LayerPainter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import IconRenderer from './IconRenderer.vue';
 import Multiblock3DViewer from './Multiblock3DViewer.vue';
 import AuthorGalleryModal from './AuthorGalleryModal.vue';
